9d290b25313ee85531e6a11f581905c49ad6b57d
Jeff Bauer <jbauer@rubic.com>
2012-02-01 20:52:11 -0600

    added AMIs for maverick, lucid

--- shaker/ami.py
+++ shaker/ami.py
@@ -92,6 +92,50 @@ ubuntu:
     us-west-2:
       i386: ami-18f97428
       x86_64: ami-1af9742a
+  maverick:
+    ap-northeast-1:
+      i386: ami-dab400db
+      x86_64: ami-dcb400dd
+    ap-southeast-1:
+      i386: ami-58215b0a
+      x86_64: ami-5e215b0c
+    eu-west-1:
+      i386: ami-020f3d76
+      x86_64: ami-0e0f3d7a
+    sa-east-1:
+      i386: ami-6235ea7f
+      x86_64: ami-9c35ea81
+    us-east-1:
+      i386: ami-f333fe9a
+      x86_64: ami-cf33fea6
+    us-west-1:
+      i386: ami-21237f64
+      x86_64: ami-23237f66
+    us-west-2:
+      x86_64: ami-06f97436
+      i386: ami-04f97434
+  lucid:
+    ap-northeast-1:
+      i386: ami-1ae6501b
+      x86_64: ami-36e65037
+    ap-southeast-1:
+      i386: ami-d8c98c8a
+      x86_64: ami-c0c98c92
+    eu-west-1:
+      i386: ami-95dde2e1
+      x86_64: ami-81dde2f5
+    sa-east-1:
+      i386: ami-6a5f8077
+      x86_64: ami-645f8079
+    us-east-1:
+      i386: ami-71dc0b18
+      x86_64: ami-55dc0b3c
+    us-west-1:
+      i386: ami-9991cfdc
+      x86_64: ami-a191cfe4
+    us-west-2:
+      i386: ami-8cb33ebc
+      x86_64: ami-8eb33ebe
 
 debian:
   squeeze:
