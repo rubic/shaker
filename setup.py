8d82b669fde2dff7f4c1353587afbb340da2349e
Alec Koumjian <akoumjian@gmail.com>
2012-05-23 14:21:23 -0700

    Installs dependencies automatically with distribute or setuptools

--- setup.py
+++ setup.py
@@ -1,21 +1,34 @@
+from __future__ import with_statement
+
 import os
-from setuptools import setup
-import shaker
+
+from distutils.core import setup
+if os.environ.get('VIRTUAL_ENV'):
+    from setuptools import setup
+
+exec(compile(open("shaker/version.py").read(), "shaker/version.py", 'exec'))
+VER = __version__
+
 
 # Utility function to read the README file.
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
+requirements = ''
+with open('requirements.txt') as f:
+    requirements = f.read()
+
 setup(
-    name = "shaker",
-    packages = ["shaker"],
-    version = shaker.__version__,
-    description = "EC2 Salt Minion Launcher",
-    author = "Jeff Bauer",
-    author_email = "jbauer@rubic.com",
-    url = "https://github.com/rubic/shaker",
-    keywords = ["salt", "ec2", "aws"],
-    classifiers = [
+    name="shaker",
+    packages=["shaker"],
+    version=VER,
+    description="EC2 Salt Minion Launcher",
+    author="Jeff Bauer",
+    author_email="jbauer@rubic.com",
+    url="https://github.com/rubic/shaker",
+    keywords=["salt", "ec2", "aws"],
+    classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
@@ -27,8 +40,9 @@ setup(
         "Topic :: System :: Distributed Computing",
         "Topic :: System :: Systems Administration",
         ],
-    long_description = read('README.rst'),
+    long_description=read('README.rst'),
     scripts=['scripts/shaker',
              'scripts/shaker-terminate',
          ],
+    install_requires=requirements,
 )
