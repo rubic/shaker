e68e399092272fa40dbeb4bf50736c79fa384903
Jeff Bauer <jbauer@rubic.com>
2012-09-29 21:55:26 -0500

    add mock modules to sys.modules

--- docs/conf.py
+++ docs/conf.py
@@ -45,6 +45,9 @@ MOCK_MODULES = [
     'M2Crypto',
 ]
 
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
